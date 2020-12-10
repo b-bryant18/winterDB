2952cbfa6e242dc3a76b6c376a27a214e022a0b2
Brad Bryant <bradleybryant86@gmail.com>
2020-12-10 00:38:46 -0600

    Added if statement to require dotenv file if not in pproduction environment

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+//Uses variables from .env file for process.env below
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
@@ -13,9 +18,11 @@ app.use(express.static('public'));
 //Use mongoose to connect to MongoDB
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 const db = mongoose.connection;
+// Log successful connection to DB or log errors if any
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
